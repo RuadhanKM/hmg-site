a484097580ac09715649e01a23d395b2a70d8fea
Ruadhan Keller-Mcleer <9281239@students.k12.dc.us>
2022-03-16 13:58:32 -0400

    changed paths

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const hostname = os.networkInterfaces()['Wi-Fi'][0].address
 chatws.main(hostname)
 tanksws.main(hostname)
 
-app.use('/chat', express.static("C:/Users/9281239/Documents/js/chat-room-2/public"))
-app.use('/games', express.static("C:/Users/9281239/Documents/js/games/public"))
+app.use('/chat', express.static("C:/Users/9281239/Documents/code/js/chat-room-2/public"))
+app.use('/games', express.static("C:/Users/9281239/Documents/code/js/games/public"))
 
 app.get('/tonl', (req, res) => {
 	res.redirect("/games/tanks-online/?ip="+req.query.ip)
@@ -22,7 +22,7 @@ app.get('/tonl', (req, res) => {
 app.use('/', express.static("public"))
 
 app.get('*', function(req, res){
-	res.status(404).sendFile('C:/Users/9281239/Documents/js/hmg-site/public/404.html')
+	res.status(404).sendFile('C:/Users/9281239/Documents/code/js/hmg-site/public/404.html')
 })
 
 app.listen(port, hostname, () => {
